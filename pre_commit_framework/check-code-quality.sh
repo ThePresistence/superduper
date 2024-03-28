5a407525b4061519107906d95ecdc486fb1ef573
ThePresistence <102717267+ThePresistence@users.noreply.github.com>
2024-03-28 12:06:22 +0000

    added

--- pre_commit_framework/check-code-quality.sh
+++ pre_commit_framework/check-code-quality.sh
@@ -2,9 +2,15 @@
 
 set -e  # If there is any error it will stop the whole bash script file :) 
 
-black --config .black.toml . 
-pylint --rcfile .pylintrc *.py 
-flake8 --config .flake8.toml
-mypy . 
-isort . --settings .isort.cfg
+
+EXIT_STATUS=0
+
+black --config .black.toml .  || ((EXIT_STATUS++))
+pylint --rcfile .pylintrc *.py  || ((EXIT_STATUS++))
+flake8 --config .flake8.toml || ((EXIT_STATUS++))
+mypy .  || ((EXIT_STATUS++))
+isort . --settings .isort.cfg || ((EXIT_STATUS++))
 # try to use ruff also If you want :) 
+
+echo existing with status $EXIT_STATUS 
+exit $EXIT_STATUS
